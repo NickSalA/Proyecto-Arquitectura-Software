a8f8d54942c5c3b73a2463a9f6ce14f1c398fb89
MathiMomos <mathi.gamer.m@gmail.com>
2026-06-16 20:06:09 -0500

    fix: change spring boot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
-    id("org.springframework.boot") version "3.2.6"
+    id("org.springframework.boot") version "3.3.6"
     id("io.spring.dependency-management") version "1.1.5"
     application
 }
